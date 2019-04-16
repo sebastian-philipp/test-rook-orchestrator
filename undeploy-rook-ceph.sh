082b0667114fc279355fd8206edb618a3f529b17
Sebastian Wagner <sebastian.wagner@suse.com>
2019-04-16 09:49:01 +0200

    Remove finalizer when undeploying.
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- undeploy-rook-ceph.sh
+++ undeploy-rook-ceph.sh
@@ -3,36 +3,42 @@ set -x
 
 . config_paths.sh
 
-crs="CephNFS CephObjectStore CephFilesystem CephCluster"
+crs="CephNFS CephObjectStore CephFilesystem CephCluster job"
 apps="mon mgr osd mds rgw tools"
+
+# finalizers can deadlock
+kubectl patch crd/cephcluster.ceph.rook.io -p '{"metadata":{"finalizers":[]}}' --type=merge
 
 for cr in $crs
 do
-    timeout 30 kubectl delete $cr --all
+    timeout 30 kubectl delete $cr --grace-period=0 --force --all
 done
 
 timeout 30 kubectl delete -f $rook_path/cluster.yaml
 timeout 30 kubectl delete -f $rook_path/toolbox.yaml
-timeout 30 kubectl delete pod -n rook-ceph-system -l app=rook-ceph-operator
-
-for cr in $(echo $crs)
-do
-    while kubectl get $cr rook-ceph ; do
-        sleep 1
-    done
-done
+timeout 30 kubectl delete pod -n rook-ceph -l app=rook-ceph-operator
 
 for primitive in service deployment pod
 do
     for app in $(echo $apps)
     do
-        timeout 30 kubectl delete $primitive -l app=rook-ceph-$app
+        timeout 30 kubectl delete $primitive --grace-period=0 --force -l app=rook-ceph-$app
     done
 done
+
+timeout 30 kubectl delete pod -l job=rook-ceph-detect-version
+timeout 30 kubectl delete pod -l job-name=rook-ceph-nfs-ganesha-rados-grace
 
 for app in $(echo $apps)
 do
     while kubectl get pod -l app=rook-ceph-$app -o json | jq -e '.items[0].metadata.labels.app' ; do
+        sleep 1
+    done
+done
+
+for cr in $(echo $crs)
+do
+    while kubectl get $cr rook-ceph ; do
         sleep 1
     done
 done
